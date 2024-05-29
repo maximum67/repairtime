1ff5a7e7bff48d9184cf3a521fc513d6859fa67b
67134 <gigalskiy@gmail.com>
2024-05-29 19:26:17 +0300

    code version 3 27.05.2024

--- src/main/java/com/example/repairtime/configuration/SecurityConfig.java
+++ src/main/java/com/example/repairtime/configuration/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers( "/css/**","/images/*","/auth/registration").permitAll()
+                .requestMatchers("/css/**","/images/*","/auth/registration").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
