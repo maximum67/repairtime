974c8d1b641c1e57bbd68443b1992db57ba85859
Gigalskiy <gigalskiy@gmail.com>
2024-06-13 16:58:17 +0300

    redaction 13.06.2024

--- src/main/java/com/example/repairtime/cipher/SecretKeyCipher.java
+++ src/main/java/com/example/repairtime/cipher/SecretKeyCipher.java
@@ -1,10 +1,26 @@
 package com.example.repairtime.cipher;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import javax.crypto.spec.SecretKeySpec;
 
+@Component
 public class SecretKeyCipher {
+
+    private static String CIPHER_KEY;
+
+    @Value("${cipherKey}")
+    public void setCipherKey(String cipherKey) {
+        CIPHER_KEY = cipherKey;
+    }
+
+    public static String getCipherKey() {
+        return CIPHER_KEY;
+    }
+
     public static SecretKeySpec getKey(){
-        return new SecretKeySpec("testtesttesttest".getBytes(), "AES");
+        return new SecretKeySpec(getCipherKey().getBytes(), "AES");
     }
 
 }
